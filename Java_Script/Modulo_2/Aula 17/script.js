ca6410440b7ec113dc309fe6b20598e24a4fb79c
Elton <eltonsouzaoliveira5@gmail.com>
2024-06-07 12:59:39 -0300

    att-diaria

--- Java_Script/Modulo_2/Aula 17/script.js
+++ Java_Script/Modulo_2/Aula 17/script.js
@@ -24,3 +24,18 @@ console.log(`Meu objeto pokemon possui Defesa: ${Pokemon.defesa}`);
 console.log(`Meu objeto pokemon possui Ataque: ${Pokemon.ataque}`);
 console.log(`Meu objeto pokemon possui Habilidade: ${Pokemon.habilidade}`);
 
+console.log('O Resultado do exercício 3 é:');
+
+const Valor1 = 12;
+const Valor2 = 45;
+
+console.log(`O resultodo da operação aritimética "+"(Soma) entre as duas variáves é: ${Valor1 + Valor2}`);
+console.log(`O resultodo da operação aritimética "-"(Subitração) entre as duas variáves é: ${Valor1 - Valor2}`);
+console.log(`O resultodo da operação aritimética "/"(Divizão) entre as duas variáves é: ${Valor1 / Valor2}`);
+console.log(`O resultodo da operação aritimética "*"(Multiplicação) entre as duas variáves é: ${Valor1 * Valor2}`);
+
+console.log('O Resultado do exercício 4 é:');
+
+console.log(`O resultodo da operação de comparação ">"(Maior que) entre as duas variáves é: ${Valor1 > Valor2}`);
+console.log(`O resultodo da operação de comparação "<"(Menor que) entre as duas variáves é: ${Valor1 < Valor2}`);
+console.log(`O resultodo da operação de comparação "==="(Igual á) entre as duas variáves é: ${Valor1 === Valor2}`);
