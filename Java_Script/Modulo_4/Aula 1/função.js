4f6b1efeb1454a0c38c9eda36d7fa1a0d2945dbc
Elton <eltonsouzaoliveira5@gmail.com>
2024-06-09 20:45:44 -0300

    Att de conteúdo

---
+++ Java_Script/Modulo_4/Aula 1/função.js
@@ -0,0 +1,14 @@
+//as 'funções' no JS tem a utilidade de agrupar um conjunto de ações para executar sempre que chamadas
+
+
+function pegarComanda() {
+    console.log('Olá, boa noite!')
+    console.log('Pegue aqui a sua comanda!')
+    console.log('Bom apetite!!')
+    console.log('-------------------------------')
+}
+
+pegarComanda();
+pegarComanda();
+pegarComanda();
+pegarComanda();
---
+++ Java_Script/Modulo_4/Aula 2/Funções - Parâmetros e Argumentos.js
@@ -0,0 +1,25 @@
+//parametro vai na função dentro dos "()" para que ela funcione
+//argumento é o valor associado ao parametro quando a função é chamada
+
+function calcularQuadrado(numeroDaVez) {
+  console.log(`O número da vez é o ${numeroDaVez}`);
+  console.log(
+    `O número da vez (${numeroDaVez}) ao quadrado vale ${numeroDaVez ** 2}`
+  );
+}
+
+calcularQuadrado(1);
+calcularQuadrado(2);
+calcularQuadrado(3);
+calcularQuadrado(4);
+calcularQuadrado(5);
+calcularQuadrado(6);
+calcularQuadrado(7);
+calcularQuadrado(8);
+calcularQuadrado(9);
+calcularQuadrado(10);
+calcularQuadrado(11);
+calcularQuadrado(12);
+calcularQuadrado(13);
+calcularQuadrado(14);
+calcularQuadrado(15);
