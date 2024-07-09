8effc35313aebff42b973c9d34d3de6f0950a86e
Elton <eltonsouzaoliveira5@gmail.com>
2024-07-09 20:14:03 -0300

    Calculate Pythagoras

--- Java_Script/Modulo_5/Aula 1/TiposDeErros.js
+++ Java_Script/Modulo_5/Aula 1/TiposDeErros.js
@@ -1,3 +1,10 @@
 function calculeSquareRoot (radicand) {
     return radicand ** (1/2);
 }
+
+function calculePythagoras(side1, side2) {
+    const sum = side1 ** 2 + side2 ** 2;
+    return calculeSquareRoot(sum);
+}
+
+console.log(`O tamanho da hipotenuza de um triângulo de lados 3 e 4 é ${calculePythagoras(3, 4)}`)
